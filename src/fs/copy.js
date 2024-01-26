7cf85fae526bf23bd725b68570b447820def3641
michaelsmorgon <smorgon@int.pl>
2024-01-26 21:01:27 +0100

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,50 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 const copy = async () => {
-    // Write your code here 
+  const errorMsg = 'FS operation failed';
+  const dirPathFrom = path.resolve('./src/fs/files');
+  const dirPathTo = path.resolve('./src/fs/files_copy');
+
+  fs.stat(dirPathFrom, (err, stats) => {
+    if (err !== null || !stats.isDirectory()) {
+      throw new Error(errorMsg);
+    }
+  });
+
+  fs.stat(dirPathTo, (err) => {
+    if (err === null) {
+      throw new Error(errorMsg);
+    }
+  });
+
+  copyRecursive(dirPathFrom, dirPathTo);
 };
+
+const copyRecursive = (pathFrom, pathTo) => {
+  fs.stat(pathFrom, (err, stats) => {
+    if (err === null && stats.isDirectory()) {
+      fs.mkdir(pathTo, (err) => {
+        throwError(err);
+      });
+      fs.readdir(pathFrom, (err, files) => {
+        throwError(err);
+        files.forEach((file) => {
+          copyRecursive(path.join(pathFrom, file), path.join(pathTo, file));
+        });
+      });
+    } else {
+      fs.copyFile(pathFrom, pathTo, (err) => {
+        throwError(err);
+      });
+    }
+  });
+}
+
+const throwError = (err) => {
+  if (err) {
+    throw new Error(errorMsg);
+  }
+}
 
 await copy();
