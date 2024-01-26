1833bc4c4eedc605652cd3f990470f8f185b6fa0
michaelsmorgon <smorgon@int.pl>
2024-01-26 21:01:56 +0100

    refactor: add semicolon

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,22 +2,22 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 const create = async () => {
-    const errorMsg = 'FS operation failed';
-    const text = 'I am fresh and young';
-    const filePath = path.resolve('./src/fs/files/fresh.txt');
+  const errorMsg = 'FS operation failed';
+  const text = 'I am fresh and young';
+  const filePath = path.resolve('./src/fs/files/fresh.txt');
 
-    fs.stat(filePath, (err) => {
-        if (err === null) {
-            throw new Error(errorMsg);
-        }
-    })
+  fs.stat(filePath, (err) => {
+    if (err === null) {
+      throw new Error(errorMsg);
+    }
+  });
 
-    fs.writeFile(filePath, text, (err) => {
-        if (err) {
-            throw new Error(errorMsg);
-        }
-        console.log('Done');
-    });
+  fs.writeFile(filePath, text, (err) => {
+    if (err) {
+      throw new Error(errorMsg);
+    }
+    console.log('Done');
+  });
 };
 
 await create();
