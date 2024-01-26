a2cde2165f06864c016947f588f4afc77aed8e0d
michaelsmorgon <smorgon@int.pl>
2024-01-26 19:25:06 +0100

    feat: add create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const errorMsg = 'FS operation failed';
+    const text = 'I am fresh and young';
+    const filePath = path.resolve('./src/fs/files/fresh.txt');
+
+    fs.stat(filePath, (err) => {
+        if (err === null) {
+            throw new Error(errorMsg);
+        }
+    })
+
+    fs.writeFile(filePath, text, (err) => {
+        if (err) {
+            throw new Error(errorMsg);
+        }
+        console.log('Done');
+    });
 };
 
 await create();
