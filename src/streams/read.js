f0a1938b72a9cb356919fd207e3b3a9bc74f7bf6
michaelsmorgon <smorgon@int.pl>
2024-01-27 22:56:56 +0100

    feat: add read stream function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import process from 'node:process';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+  fs.createReadStream(filePath)
+    .on('data', (chunk) => {
+      process.stdout.write(chunk.toString());
+    });
 };
 
 await read();
