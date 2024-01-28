e7911ebd17091dc9e2438449d6d7f2b1646853fb
michaelsmorgon <smorgon@int.pl>
2024-01-28 14:08:58 +0100

    feat: add worker function

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,29 @@
+import process from 'node:process';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  console.log('Please enter a Natural Number: ');
+
+  // Begin reading from stdin so the process does not exit.
+  process.stdin.on('data', (chunk) => {
+    const data = chunk.toString().trim();
+    if (!data) {
+      return;
+    }
+    let isNumber = !/[^\d]+/g.test(data);
+
+    if (!isNumber) {
+      console.log('You should enter a Natural Number: ');
+      return;
+    }
+    process.stdout.write('--------------------\n');
+    console.log('You entered: ' + data);
+    process.stdout.write(`Fibonacci result: ${nthFibonacci(data)}\n`);
+    process.stdout.write('--------------------\n');
+    process.stdout.write('Please enter a Natural Number: ');
+  });
 };
 
 sendResult();
