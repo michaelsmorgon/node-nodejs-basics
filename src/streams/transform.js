448d682a81954304529dec4c4c46e230c3446d28
michaelsmorgon <smorgon@int.pl>
2024-01-28 12:24:12 +0100

    feat: add transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import process from 'node:process';
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const transf = new Transform({
+    transform(chunk, enc, callback) {
+      const reverse = chunk.toString().trim().split('').reverse().join('');
+      callback(null, reverse + '\n');
+    }
+  });
+
+  pipeline(
+    process.stdin,
+    transf,
+    process.stdout,
+    (err) => {
+      console.log(err);
+    }
+  )
 };
 
 await transform();
