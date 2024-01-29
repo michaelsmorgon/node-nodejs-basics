4050cd887c23cd90b7f2d14d354877c5e0e53900
michaelsmorgon <smorgon@int.pl>
2024-01-29 18:39:01 +0100

    feat: add Worker Threads function

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import os from 'os';
+import { Worker } from 'node:worker_threads';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
 const performCalculations = async () => {
-    // Write your code here
+  let numberFib = 10;
+  const cpus = os.cpus();
+  const numbersOfCore = cpus.length;
+
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, 'worker.js');
+
+  Promise.all(
+    Array(numbersOfCore).fill().map((val, ind) => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(filePath, {
+          workerData: {
+            data: numberFib++,
+            index: ind
+          }
+        });
+        worker.on('message', (res) => {
+          resolve(res);
+        });
+      });
+    })
+  )
+  .then((workerResult) => {
+    console.log(workerResult);
+  });
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,29 +1,33 @@
-import process from 'node:process';
+import {
+  parentPort, workerData,
+} from 'node:worker_threads';
 
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  console.log('Please enter a Natural Number: ');
-
-  // Begin reading from stdin so the process does not exit.
-  process.stdin.on('data', (chunk) => {
-    const data = chunk.toString().trim();
-    if (!data) {
-      return;
+  let res = {};
+  const data = workerData.data.toString().trim();
+  if (!data) {
+    res = {
+      status: 'error',
+      data: null
     }
-    let isNumber = !/[^\d]+/g.test(data);
+  }
+  let isNumber = !/[^\d]+/g.test(data);
 
-    if (!isNumber) {
-      console.log('You should enter a Natural Number: ');
-      return;
+  if (!isNumber) {
+    res = {
+      status: 'error',
+      data: null
     }
-    process.stdout.write('--------------------\n');
-    console.log('You entered: ' + data);
-    process.stdout.write(`Fibonacci result: ${nthFibonacci(data)}\n`);
-    process.stdout.write('--------------------\n');
-    process.stdout.write('Please enter a Natural Number: ');
-  });
+  } else {
+    res = {
+      status: 'resolved',
+      data: nthFibonacci(workerData.data)
+    }
+  }
+  parentPort.postMessage(res);
 };
 
 sendResult();
