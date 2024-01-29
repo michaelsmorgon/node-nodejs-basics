d10dcba9b551e412b10470bb478f21a8694af3d4
michaelsmorgon <smorgon@int.pl>
2024-01-29 20:56:25 +0100

    feat: add Child Processes function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, 'files', 'script.js');
+
+  const child = fork(filePath, args);
+  child.on('message', (message) => {
+    console.log(`Main received message: ${message}`);
+  });
+  child.send(args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([5, 20, 30]);
